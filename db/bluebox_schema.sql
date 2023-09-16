7aa464e325aca0653c61a7923078e2901c728ab8
igao <nascimentoigor1311@gmail.com>
2023-09-16 15:08:51 -0300

    arrumando schema

--- db/bluebox_schema.sql
+++ db/bluebox_schema.sql
@@ -20,12 +20,12 @@ create table tb_admin (
 
 create table tb_endereco (
 	id_endereco 		int primary key not null auto_increment,
-	ds_logradouro 		varchar not null,
-	ds_bairro 			varchar not null,
-	ds_cidade 			varchar not null,
-	ds_cep 				varchar not null,
+	ds_logradouro 		varchar(200) not null,
+	ds_bairro 			varchar(200) not null,
+	ds_cidade 			varchar(200) not null,
+	ds_cep 				varchar(100) not null,
 	nr_endereco 		int not null,
-	ds_complemento 		varchar not null
+	ds_complemento 		varchar(20) not null
 );
 
 create table tb_cliente (
@@ -34,12 +34,12 @@ create table tb_cliente (
 	ds_sobrenome 		varchar(100) not null,
 	ds_email 			varchar(100) not null,
 	ds_senha 			varchar(100) not null,
-	dt_cadastro 		date not null,
 	ds_telefone 		varchar(100) not null,
 	ds_cpf 				varchar(100) not null,
-	id_endereco 		varchar(100) not null,
-	dt_nascimento 		date(2023) not null,
+	id_endereco 		int,
+	dt_nascimento 		date not null,
 	img_perfil 			varchar not null,
+	dt_cadastro 		date not null,
 	foreign key (id_endereco) references tb_endereco(id_endereco),
 );
 
@@ -48,25 +48,25 @@ create table tb_cliente (
 
 create table tb_categoria (
 	id_categoria 		int primary key not null auto_increment,
-	ds_categoria 		varchar not null
+	ds_categoria 		varchar(200) not null
 );
 
 -- A tabela categorias deve conter categorias como "Jogos para PC", "Jogos para Xbox", além de "Consoles" e "Action Figures", para evitar criar uma "tabela plataformas"
 
 create table tb_marca (
 	id_marca			int primary key not null auto_increment,
-	ds_marca 			varchar not null
+	ds_marca 			varchar(200) not null
 );
 
 create table tb_produto (
 	id_produto 			int primary key not null auto_increment,
-	nm_produto 			varchar not null,
+	nm_produto 			varchar(300) not null,
 	vl_preco 			numeric not null,
 	vl_promocional		numeric not null,
 	bt_promocao 		boolean not null,
 	qtd_estoque 		numeric not null,
-	ds_produto 			varchar not null,
-	ds_especificacoes 	varchar not null,
+	ds_produto 			varchar(800) not null,
+	ds_especificacoes 	varchar(400) not null,
 	id_categoria 		int not null,
 	id_marca 			int not null,
 	bt_usado 			boolean not null,
@@ -88,7 +88,7 @@ create table tb_comentario (
 	id_comentario 		int primary key not null auto_increment,
 	id_produto 			int not null,
 	id_cliente 			int not null,
-	ds_comentario 		varchar not null,
+	ds_comentario 		varchar(500) not null,
 	ds_nota 			numeric not null,
 	dt_postagem 		datetime not null,
 	nr_likes 			int not null,
@@ -98,7 +98,7 @@ create table tb_comentario (
 
 create table tb_cupom (
 	id_cupom 			int primary key not null auto_increment,
-	ds_cupom 			varchar not null,
+	ds_cupom 			varchar(30) not null,
 	vl_desconto 		decimal not null,
 	dt_expiracao 		date not null
 );
@@ -124,7 +124,7 @@ create table tb_pedido (
 	id_cliente 			int not null,
 	id_produto 			int not null,
 	vl_preco_total 		numeric not null,
-	id_estado 			varchar not null,
+	id_estado 			int not null,
 	id_tipo_pagamento 	int not null,
 	dt_compra 			date not null,
 	dt_aprovacao 		date not null,
