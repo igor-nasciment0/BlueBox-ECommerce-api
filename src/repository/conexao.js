24ba8d70c4c0e69f90f0694d468df04fc1c95cf3
HenriSilva11 <pequenohenri72@gmail.com>
2023-10-27 15:17:02 -0300

    okok

--- src/repository/conexao.js
+++ src/repository/conexao.js
@@ -2,10 +2,10 @@ import 'dotenv/config'
 import mysql from 'mysql2/promise';
 
 let dados = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PWD,
-    database: process.env.DB,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DB,
     typeCast: function (field, next) {
         if (field.type === 'TINY' && field.length === 1) {
             return field.string() === '1'
